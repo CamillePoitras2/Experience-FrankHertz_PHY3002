fb8b4fe25413c3c8d08bb291b0bd1cfe077bb08a
Camille Poitras <108810834+CamillePoitras2@users.noreply.github.com>
2023-10-31 08:50:59 -0400

    Update analyse_legere_pic_multiple.py

--- analyse_legere_pic_multiple.py
+++ analyse_legere_pic_multiple.py
@@ -1,108 +1,59 @@
-"""
-------------------------------------------------------------------------------
-
-Pour vous faciliter la vie, vous pouvez utiliser le code présent dans le dossier
-outils_analyse.
-
-_____________________________________________________________________________
-"""
-"""
-------------------------------------------------------------------------------
-
-Mettres vois import ici
-
-_____________________________________________________________________________
-"""
-
+from outils_analyse.identification_des_pics import determiner_indexes_maximums_scipy
+from outils_analyse.lecture_des_fichiers import lire_csv_a_3_colonnes, crop_pour_conserver_que_la_partie_avec_rampe
+from outils_analyse.conversion_temps_en_potentiel import \
+        calculer_facteur_conversion_temps_en_potentiel_avec_mesure_rampe
+from outils_analyse.fits import gaussian_fit, gaus, linear_regression, round_any
 import matplotlib.pyplot as plt
+import numpy as np
+import os
 import matplotlib
 
-
-
-
-# Cette ligne contrôle le font size pour les écritures sur le graphique
+# Fixer la taille de la police dans les figures
 matplotlib.rcParams.update({'font.size': 18})
-"""
-------------------------------------------------------------------------------
 
-Lire le fichier des résultats et transformer les valeurs en un array numpy
+# -------------------------------------------------------------------------------------------------
+#                        Importer les données et leurs paramètres respectifs 
+# -------------------------------------------------------------------------------------------------
+# Spécifier le nom du fichier .csv
+csv_file_name = ['Montage_B_step6.4']
+csv_file_path = os.path.join("data", csv_file_name)
+valeurs_en_array = lire_csv_a_3_colonnes(csv_file_path, 9)
 
-_____________________________________________________________________________
-"""
-# Mettre votre code ici
+# -------------------------------------------------------------------------------------------------
+#            Retirer les valeurs à l'extérieur de l'activation du générateur de rampe
+# -------------------------------------------------------------------------------------------------
+# Retrait des valeurs à l'extérieur 
+valeurs_cropped_debutant_par_t0 = crop_pour_conserver_que_la_partie_avec_rampe(valeurs_en_array,
+                                                                               2, 0.05, 0.1)
+# Réétablir les données tronquées pour débuter à t_0=0
+valeurs_cropped_debutant_par_t0[:, 0] -= np.min(valeurs_cropped_debutant_par_t0[:, 0])
 
+# -------------------------------------------------------------------------------------------------
+#          Calculer la pente de la tension du générateur de rampe et son incertitude
+# -------------------------------------------------------------------------------------------------
+# Calculer la pente de la tension du générateur de rampe et son incertitude.
+facteur_valeur, facteur_incertitude = calculer_facteur_conversion_temps_en_potentiel_avec_mesure_rampe(valeurs_cropped_debutant_par_t0,
+                                                                                                       0, 2)
+print("Pente = ", f"{facteur_valeur} +- {facteur_incertitude}") # Afficher les valeurs
 
+# Convertir les valeurs de temps en valeurs de tension
+valeurs_avec_bonnes_unites = valeurs_cropped_debutant_par_t0.copy()
+valeurs_avec_bonnes_unites[:, 0] = -facteur_valeur * valeurs_cropped_debutant_par_t0[:, 0]
 
-
-
-
-
-
-"""
-------------------------------------------------------------------------------
-
-Retirer les valeurs qui se trouvent à l'extérieur de l'activation du générateur de rampe.
-Ne pas oublier de mettre le début de la rampe comme étant t=0.
-
-_____________________________________________________________________________
-"""
-# Mettre votre code ici
-
-
-
-
-
-
-"""
-------------------------------------------------------------------------------
-
-Calculer la pente de la tension du générateur de rampe et son incertitude.
-Afficher cette valeur et son incertitude, puis convertir les valeurs de temps
-en valeurs de tension.
-
-Ensuite, convertissez les valeurs de tensions du pico en valeur de courant, en considérant que l'échelle
-du pico utilisé est de 3nA. 
-_____________________________________________________________________________
-"""
-# Mettre votre code ici
-
-
-
-
-"""
-------------------------------------------------------------------------------
-
-Déterminer l'emplacement approximatif des maximums. Ça devrait être
-environ: Estimation des pics: [ 1.5860128  3.7164788  6.7701464  8.569206  10.486626  14.676541
- 16.499271 ] V
-
-_____________________________________________________________________________
-"""
-# Mettre votre code ici
-
-
-
-
-
-
-
-
-
-
-
-
+# -------------------------------------------------------------------------------------------------
+#                 Déterminer les positions approximatives des maximums (pics)
+# -------------------------------------------------------------------------------------------------
 # Mettre vos données avec les bonnes unités à la place du None
-valeurs_avec_bonnes_unites_determination_des_pics = None  # Array de trois colonnes
-liste_des_indexes_des_pics = None  # Liste de nombres entiers
+valeurs_avec_bonnes_unites_determination_des_pics = valeurs_avec_bonnes_unites.copy()
+liste_des_indexes_des_pics =  determiner_indexes_maximums_scipy(valeurs_avec_bonnes_unites, 1,
+                                                                distance_minumum=100)
+# Obtenir les données pour les indices incluant les pics
+valeurs_avec_bonnes_unites_peaks = valeurs_avec_bonnes_unites[liste_des_indexes_des_pics]
 
-"""
-_____________________________________________________________________________
-"""
-# Ne pas modifier cette section!!!
+print("Estimation des pics:", valeurs_avec_bonnes_unites_determination_des_pics[liste_des_indexes_des_pics,
+                                                                                0])
 
-print("Estimation des pics:", valeurs_avec_bonnes_unites_determination_des_pics[liste_des_indexes_des_pics, 0])
-
-# La figure obtenue devrait correspondre à celle de figures_exemple/estimation_des_pics_multi_pics
+# Afficher les données du courant du pico en fonction de la tension entre G1 et le ground
 plt.figure()
 plt.plot(valeurs_avec_bonnes_unites_determination_des_pics[:, 0],
          valeurs_avec_bonnes_unites_determination_des_pics[:, 1],
